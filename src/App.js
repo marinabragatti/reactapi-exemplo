b5ed0c05386eae2f3b58fbf9a8a93fde9cf45dc5
Marina Bragatti Barbosa <marinabragatti@hotmail.com>
2020-03-13 14:44:27 -0300

    Exemplo React

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './style.css';
+
+import Header from './components/header';
+import Main from './pages/main';
+
+const App = () => (
+    <div className="App">
+      <Header/>  
+      <Main/>
+    </div>
+  );
+
+export default App;
