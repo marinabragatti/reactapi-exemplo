3b9c69ccff55462ca6b03210f184d9192ca9d9ab
Marina Bragatti Barbosa <marinabragatti@hotmail.com>
2020-03-16 11:49:53 -0300

    Exemplo consumo API com React finalizado

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
+import { Link } from 'react-router-dom';
 import './styles.css';
 
 export default class Main extends Component {
@@ -14,19 +15,27 @@ export default class Main extends Component {
     }
 
     loadProducts = async (page = 1) => {
-        const response = await api.get('/products?page=${page}');
+        const response = await api.get(`/products?page=${page}`);
 
         const { docs, ...productInfo } = response.data;
 
-        this.setState({ products: docs, productInfo});
+        this.setState({ products: docs, productInfo, page});
     };
 
-    prevPage = () => {};
+    prevPage = () => {
+        const { page } = this.state;
+
+        if(page === 1) return;
+
+        const pageNumber = page-1;
+
+        this.loadProducts(pageNumber);
+    };
 
     nextPage = () => {
         const { page, productInfo } = this.state;
 
-        if(page === productInfo.page) return;
+        if(page === productInfo.pages) return;
 
         const pageNumber = page+1;
 
@@ -34,7 +43,7 @@ export default class Main extends Component {
     };
 
     render() {
-        const {products} = this.state;
+        const {products, page, productInfo} = this.state;
         return (
 
             <div className="product-list">
@@ -42,12 +51,12 @@ export default class Main extends Component {
                     <article key={product._id}>
                         <strong>{product.title}</strong>
                         <p>{product.description}</p>
-                        <a href="">Acessar</a>
+                        <Link to={`/products/${product._id}`}>Acessar</Link>
                     </article>
                 ))}
                 <div className="actions">
-                    <button onClick={this.prevPage}>Anterior</button>
-                    <button onClick={this.nextPage}>Próxima</button>
+                    <button disabled={page === 1} onClick={this.prevPage}>Anterior</button>
+                    <button disabled={page === productInfo.pages} onClick={this.nextPage}>Próxima</button>
                 </div>
             </div>
         )
